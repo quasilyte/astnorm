2ec3b995e7cce49f7c27c13a555b94e9d5b6ac73
Iskander Sharipov <quasilyte@gmail.com>
2019-01-24 13:07:19 +0300

    make a copy of input arguments
    It can be simpler to do some rewrites by doing mutations,
    so make an eager deep copy to avoid accidental side effects.
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- astnorm.go
+++ astnorm.go
@@ -3,6 +3,8 @@ package astnorm
 import (
 	"go/ast"
 	"go/types"
+
+	"github.com/go-toolsmith/astcopy"
 )
 
 type Config struct {
@@ -10,5 +12,6 @@ type Config struct {
 }
 
 func Expr(cfg *Config, x ast.Expr) ast.Expr {
-	return newNormalizer(cfg).normalizeExpr(x)
+	copied := astcopy.Expr(x)
+	return newNormalizer(cfg).normalizeExpr(copied)
 }
