6acc8caf988bf405a03ef23efa516f68d2618acb
Iskander Sharipov <quasilyte@gmail.com>
2019-01-24 13:17:26 +0300

    Revert "make a copy of input arguments"
    Copying erases types info.
    Will stick to modifying in-place for now.
    
    This reverts commit 2ec3b995e7cce49f7c27c13a555b94e9d5b6ac73.

--- astnorm.go
+++ astnorm.go
@@ -3,8 +3,6 @@ package astnorm
 import (
 	"go/ast"
 	"go/types"
-
-	"github.com/go-toolsmith/astcopy"
 )
 
 type Config struct {
@@ -12,6 +10,5 @@ type Config struct {
 }
 
 func Expr(cfg *Config, x ast.Expr) ast.Expr {
-	copied := astcopy.Expr(x)
-	return newNormalizer(cfg).normalizeExpr(copied)
+	return newNormalizer(cfg).normalizeExpr(x)
 }
